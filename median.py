8113484a4dcb48880c3f575eddd820ddfd624041
Paul Cheung <cheungkuensum@gmail.com>
2022-09-29 18:03:56 +0100

    Add a bracket to fix the bug

--- median.py
+++ median.py
@@ -12,6 +12,6 @@ while True:
 numbers.sort()
 length = len(numbers)
 if(length%2==0):
-    print (numbers[length // 2 - 1] + numbers[length // 2]/2)
+    print ((numbers[length // 2 - 1] + numbers[length // 2])/2)
 else:
     print (numbers[(length-1)//2])
